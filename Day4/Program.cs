2620ea82ea5486cddb29ef1d25cafbb4d3aa5a63
lavssubbu <63305669+lavssubbu@users.noreply.github.com>
2025-01-30 12:53:03 +0530

    getandsetmethods

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,36 +1,19 @@
-﻿namespace MthdOverloading
+﻿namespace getsettermthdsample
 {
-    class AddOpe
-    {
-        public void Add(int a,int b)
-        {
-            Console.WriteLine(a + b);
-        }
-        public static void Add(double a,double b)
-        {
-            Console.WriteLine(a + b);
-        }
-        public static void Add(int a, int b,int c)
-        {
-            Console.WriteLine(a + b);
-        }
-        public string Add(string a,string b)
-        {
-            return a + b;
-        }
-        public void Add(double c,float d)
-        {
-            Console.WriteLine(c + d);
-        }
-    }
     internal class Program
     {
         private static void Main(string[] args)
         {
-           AddOpe op = new AddOpe();
-            Console.WriteLine(op.Add("csharp", "training"));
-            AddOpe.Add(10, 20, 30);              
 
+            Student stu = new Student();
+            stu.Stuid = 11;
+            stu.Name = "Anupriya";
+            stu.Age = 4;
+            Console.WriteLine(stu.Stuid);
+            Console.WriteLine(stu.Name);
+            Console.WriteLine(stu.Age);
+            //stu.SId = 111;//set accessor will invoke
+            //Console.WriteLine(stu.SId);//get accessot will invoke
         }
     }
 }
