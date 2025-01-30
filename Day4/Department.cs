292134ccc1f83e33a3d2c8cb40811db7d19862f9
lavssubbu <63305669+lavssubbu@users.noreply.github.com>
2025-01-30 10:54:24 +0530

    Day4 OOPS Concepts - Inheritance

---
+++ Day4/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiproOOPSconcepts
+{
+    internal class Department : Employee // Inheritance - Department is derived from the base class Employee
+    {
+        public string DeptName { get; set; }
+        public decimal Salary {  get; set; }
+
+        //public void show()
+        //{
+        //    Console.WriteLine(DeptName + " " + Salary);
+        //}
+        public override void Display()
+        {
+            base.Display();//control goes to the base class(Employee) display method
+            Console.WriteLine(DeptName + " " + Salary);
+        }
+    }
+}
---
+++ Day4/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiproOOPSconcepts
+{
+    internal class Employee //Base class/Parent class
+    {
+        public int Empid { get; set; }
+        public string EmpName { get; set; }
+
+        public virtual void Display()
+        {
+            Console.WriteLine(Empid+ " " + EmpName);
+        }
+    }
+}
